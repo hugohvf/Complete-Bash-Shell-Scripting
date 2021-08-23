3649a09e30daa905bf9a9a9e42dd9a222b4a9eb4
Hugo Fusinato <hugohvf@hotmail.com>
2021-08-22 22:01:17 -0300

    fix mistakes

--- Examples/report.sh
+++ Examples/report.sh
@@ -2,7 +2,7 @@
 DockerVersion=$(docker -v | cut -d " " -f 3 | tr "," " ")
 NginxVersion=$(nginx -v 2>&1 | cut -d " " -f 3 | cut -d "/" -f 2)
 OSUname=$(uname)
-cat <<EOF | /usr/bin/mail -s "$(date '+%_%m_%Y'): Report on $(hostname)" "hugohvf@hotmail.com"
+cat <<EOF | /usr/bin/mail -s "$(date '+%d_%m_%Y'): Report on $(hostname)" "hugohvf@hotmail.com"
 Docker Version: $DockerVersion
 Nginx Version: $NginxVersion
 OS Uname: $OSUname
